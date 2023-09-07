e98712d90f1dba31729cfb6a8f8914c086428769
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-07 16:11:13 +0200

    [add]: age verify for discount and message print

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//1+2
 const age = parseInt(prompt('Quanti anni hai?'));
 const userKm = parseInt(prompt('Quanti km devi percorrere?'));
 
@@ -7,16 +8,23 @@ const juniorDiscount = 0.2;
 const seniorDiscount = 0.4;
 
 //4.
-let costTicketNormal, costTicketTotal, message;
+let message, costTicketNormal, costTicketTotal;
+
+message = "Il costo del tuo biglietto è di ";
 
 costTicketNormal = (userKm * priceKm);
 
+//5.
 if (age <= 17) {
-  costTicketTotal = Math.abs((costTicketNormal * juniorDiscount) - costTicketNormal); 
+  costTicketTotal = Math.abs((costTicketNormal * juniorDiscount) - costTicketNormal).toFixed(2);
+  message += costTicketTotal + " €. Ed è stato applicato uno sconto del 20%.";
 } else if (age >= 65) {
-  costTicketTotal = Math.abs((costTicketNormal * seniorDiscount) - costTicketNormal);
+  costTicketTotal = Math.abs((costTicketNormal * seniorDiscount) - costTicketNormal).toFixed(2);
+  message += costTicketTotal + " €. Ed è stato applicato uno sconto del 40%.";
 } else {
   costTicketTotal = costTicketNormal;
+  message += costTicketTotal + " €.";
 }
 
-//5.
+//6.
+document.getElementById('output').innerHTML = message;
