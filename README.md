48ff13f9444cabe420b9e3caf47e051e86b51050
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-07 15:55:17 +0200

    [add]:ticket and discount ticket cost;[edit]README

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Calcolo del prezzo del biglietto del treno
 1. Chiedo all'utente i km da percorrere e l'età
 2. Salvo questi dati nelle rispettive variabili
 3. Dichiaro le variabili per il prezzo in € a km, e i due sconti età
-4. Dichiaro la variabile del prezzo del biglietto e lo calcolo
+4. Dichiaro le variabili del messaggio e del prezzo del biglietto e lo calcolo
 5. Verifico se c'è uno sconto da applicare e nel caso calcolo il risultato
 5. Stampo il risultato in pagina
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const age = 25//prompt('Quanti anni hai?');
-const userKm = 3//parseInt(prompt('Quanti km devi percorrere?'));
+const age = parseInt(prompt('Quanti anni hai?'));
+const userKm = parseInt(prompt('Quanti km devi percorrere?'));
 
 //3.
 const priceKm = 0.21;
@@ -7,8 +7,16 @@ const juniorDiscount = 0.2;
 const seniorDiscount = 0.4;
 
 //4.
-let costTicketNormal, costTicketTotal;
+let costTicketNormal, costTicketTotal, message;
 
 costTicketNormal = (userKm * priceKm);
 
-console.log(costTicketNormal);
+if (age <= 17) {
+  costTicketTotal = Math.abs((costTicketNormal * juniorDiscount) - costTicketNormal); 
+} else if (age >= 65) {
+  costTicketTotal = Math.abs((costTicketNormal * seniorDiscount) - costTicketNormal);
+} else {
+  costTicketTotal = costTicketNormal;
+}
+
+//5.
