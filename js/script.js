e34c4551d919bf36499bf40a65090d5a56a288d0
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-07 16:16:10 +0200

    [add]: real age verify (-1, 0, 1)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const age = parseInt(prompt('Quanti anni hai?'));
 const userKm = parseInt(prompt('Quanti km devi percorrere?'));
 
+
+
 //3.
 const priceKm = 0.21;
 const juniorDiscount = 0.2;
@@ -10,21 +12,32 @@ const seniorDiscount = 0.4;
 //4.
 let message, costTicketNormal, costTicketTotal;
 
-message = "Il costo del tuo biglietto è di ";
 
-costTicketNormal = (userKm * priceKm);
-
-//5.
-if (age <= 17) {
-  costTicketTotal = Math.abs((costTicketNormal * juniorDiscount) - costTicketNormal).toFixed(2);
-  message += costTicketTotal + " €. Ed è stato applicato uno sconto del 20%.";
-} else if (age >= 65) {
-  costTicketTotal = Math.abs((costTicketNormal * seniorDiscount) - costTicketNormal).toFixed(2);
-  message += costTicketTotal + " €. Ed è stato applicato uno sconto del 40%.";
+let ageVerify = Math.sign(age);
+if (ageVerify === -1) {
+  message = "Inserisci un'età vera"
+} else if (ageVerify === 0 ) {
+  message = "Sei appena nato?"
 } else {
-  costTicketTotal = costTicketNormal;
-  message += costTicketTotal + " €.";
+  message = "Il costo del tuo biglietto è di ";
+
+  costTicketNormal = (userKm * priceKm);
+  
+  //5.
+  if (age <= 17) {
+    costTicketTotal = Math.abs((costTicketNormal * juniorDiscount) - costTicketNormal).toFixed(2);
+    message += costTicketTotal + " €. Ed è stato applicato uno sconto del 20%.";
+  } else if (age >= 65) {
+    costTicketTotal = Math.abs((costTicketNormal * seniorDiscount) - costTicketNormal).toFixed(2);
+    message += costTicketTotal + " €. Ed è stato applicato uno sconto del 40%.";
+  } else {
+    costTicketTotal = costTicketNormal;
+    message += costTicketTotal + " €.";
+  }
 }
+
+
+
 
 //6.
 document.getElementById('output').innerHTML = message;
